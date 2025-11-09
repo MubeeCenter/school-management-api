5ef87215d96ec0e2d8f38e38afa153c312434942
MubeeCenter <muburarbash280@gmail.com>
2025-11-09 09:37:43 +0100

    create tables on startup

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from app.api.v1 import auth
 from app.api.v1 import students, courses, auth, analytics
+from app.db.sql_db import Base, engine
+from app.models.sqlalchemy_models import *   # import all models
 
 app = FastAPI(
     title="School Management API",
@@ -14,6 +16,10 @@ app.include_router(auth.router)
 app.include_router(students.router)
 app.include_router(courses.router)
 app.include_router(analytics.router)
+
+@app.on_event("startup")
+def create_tables():
+    Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 def root():
